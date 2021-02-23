c67e26bc974db4228928b984fc688636c13daca8
whwlsfb <whwlsfb@wanghw.cn>
2021-02-23 10:45:06 +0800

    fix rsa data length limit (#10)

--- src/burp/rsa/RsaUtil.java
+++ src/burp/rsa/RsaUtil.java
@@ -16,6 +16,7 @@ import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 import java.security.spec.RSAPrivateKeySpec;
 import java.security.spec.RSAPublicKeySpec;
+import java.util.Arrays;
 
 import static burp.utils.Utils.base64;
 import static burp.utils.Utils.hex;
@@ -37,10 +38,25 @@ public class RsaUtil {
         outFormat = config.OutFormat;
     }
 
-    public String encrypt(byte[] plaintext) throws Exception {
+    public String encrypt(byte[] inputArray) throws Exception {
         cipher.init(Cipher.ENCRYPT_MODE, key);
-        byte[] encrypted = cipher.doFinal(plaintext);
-        return outFormat == OutFormat.Base64 ? base64(encrypted) : hex(encrypted);
+        int MAX_ENCRYPT_BLOCK = 117;
+        int offSet = 0;
+        int inputLength = inputArray.length;
+        byte[] resultBytes = {};
+        byte[] cache = {};
+        while (inputLength - offSet > 0) {
+            if (inputLength - offSet > MAX_ENCRYPT_BLOCK) {
+                cache = cipher.doFinal(inputArray, offSet, MAX_ENCRYPT_BLOCK);
+                offSet += MAX_ENCRYPT_BLOCK;
+            } else {
+                cache = cipher.doFinal(inputArray, offSet, inputLength - offSet);
+                offSet = inputLength;
+            }
+            resultBytes = Arrays.copyOf(resultBytes, resultBytes.length + cache.length);
+            System.arraycopy(cache, 0, resultBytes, resultBytes.length - cache.length, cache.length);
+        }
+        return outFormat == OutFormat.Base64 ? base64(resultBytes) : hex(resultBytes);
     }
 
     private IllegalStateException fail(Exception e) {
