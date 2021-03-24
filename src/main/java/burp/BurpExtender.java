8ce9e868ff530abfb597efa2966719b0e53afb79
whwlsfb <whwlsfb@wanghw.cn>
2021-03-24 09:47:26 +0800

    change to maven project.

---
+++ src/main/java/burp/BurpExtender.java
@@ -0,0 +1,127 @@
+package burp;
+
+import burp.aes.AesUIHandler;
+import burp.des.DesUIHandler;
+import burp.execjs.JsUIHandler;
+import burp.rsa.RsaUIHandler;
+import burp.utils.BurpCryptoMenuFactory;
+import burp.utils.BurpStateListener;
+import burp.utils.DictLogManager;
+import org.iq80.leveldb.DB;
+import org.iq80.leveldb.Options;
+
+import javax.script.ScriptException;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+import static org.iq80.leveldb.impl.Iq80DBFactory.factory;
+
+public class BurpExtender implements IBurpExtender, ITab {
+
+    public IExtensionHelpers helpers;
+    public IBurpExtenderCallbacks callbacks;
+    public PrintWriter stdout;
+    public PrintWriter stderr;
+    public DB store;
+    public DictLogManager dict;
+    public String version = "0.1.8";
+    public HashMap<String, IIntruderPayloadProcessor> IPProcessors = new HashMap<>();
+
+    public JTabbedPane mainPanel;
+
+    public JPanel aesPanel;
+    public AesUIHandler AesUI;
+
+
+    public JPanel rsaPanel;
+    public RsaUIHandler RsaUI;
+
+    public JPanel desPanel;
+    public DesUIHandler DesUI;
+
+    public JPanel execJsPanel;
+    public JsUIHandler JsUI;
+    
+    public boolean RegIPProcessor(String extName, IIntruderPayloadProcessor processor) {
+        if (IPProcessors.containsKey(extName)) {
+            JOptionPane.showMessageDialog(mainPanel, "This name already exist!");
+            return false;
+        }
+        callbacks.registerIntruderPayloadProcessor(processor);
+        IPProcessors.put(extName, processor);
+        return true;
+    }
+
+    public void RemoveIPProcessor(String extName) {
+        if (IPProcessors.containsKey(extName)) {
+            IIntruderPayloadProcessor processor = IPProcessors.get(extName);
+            callbacks.removeIntruderPayloadProcessor(processor);
+            IPProcessors.remove(extName);
+        }
+    }
+
+    @Override
+    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks)  {
+        this.callbacks = callbacks;
+        this.helpers = callbacks.getHelpers();
+        this.stdout = new PrintWriter(callbacks.getStdout(), true);
+        this.stderr = new PrintWriter(callbacks.getStderr(), true);
+        callbacks.setExtensionName("BurpCrypto v" + version);
+        callbacks.registerExtensionStateListener(new BurpStateListener(this));
+        callbacks.registerContextMenuFactory(new BurpCryptoMenuFactory(this));
+        Options options = new Options();
+        options.createIfMissing(true);
+        try {
+            this.store = factory.open(new File("BurpCrypto.ldb"), options);
+            this.dict = new DictLogManager(this);
+            callbacks.issueAlert("LevelDb init success!");
+        } catch (IOException e) {
+            callbacks.issueAlert("LevelDb init failed! error message: " + e.getMessage());
+        }
+
+        stdout.println("BurpCrypto loaded successfully!\r\n");
+        stdout.println("Anthor: Whwlsfb");
+        stdout.println("Email: whwlsfb@wanghw.cn");
+        stdout.println("Github: https://github.com/whwlsfb/BurpCrypto");
+        InitUi();
+    }
+
+    private void InitUi() {
+        this.AesUI = new AesUIHandler(this);
+        this.RsaUI = new RsaUIHandler(this);
+        this.JsUI = new JsUIHandler(this);
+        this.DesUI = new DesUIHandler(this);
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                BurpExtender bthis = BurpExtender.this;
+                bthis.mainPanel = new JTabbedPane();
+                bthis.aesPanel = AesUI.getPanel();
+                bthis.rsaPanel = RsaUI.getPanel();
+                bthis.desPanel = DesUI.getPanel();
+                bthis.execJsPanel = JsUI.getPanel();
+                bthis.mainPanel.addTab("AES", bthis.aesPanel);
+                bthis.mainPanel.addTab("RSA", bthis.rsaPanel);
+                bthis.mainPanel.addTab("DES", bthis.desPanel);
+                bthis.mainPanel.addTab("Exec Js", bthis.execJsPanel);
+                bthis.callbacks.addSuiteTab(bthis);
+            }
+        });
+    }
+
+
+    @Override
+    public String getTabCaption() {
+        return "BurpCrypto";
+    }
+
+    @Override
+    public Component getUiComponent() {
+        return this.mainPanel;
+    }
+}
---
+++ src/main/java/burp/rsa/RsaIntruderPayloadProcessor.java
@@ -0,0 +1,36 @@
+package burp.rsa;
+
+import burp.BurpExtender;
+import burp.IIntruderPayloadProcessor;
+
+public class RsaIntruderPayloadProcessor implements IIntruderPayloadProcessor {
+    private BurpExtender parent;
+    private final String extName;
+    private final RsaUtil RsaUtil;
+
+    public RsaIntruderPayloadProcessor(final BurpExtender newParent, String extName, RsaConfig config) {
+        this.parent = newParent;
+        this.extName = extName;
+        RsaUtil = new RsaUtil();
+        RsaUtil.setConfig(config);
+    }
+
+    @Override
+    public String getProcessorName() {
+        return "BurpCrypto - RSA Encrypt - " + extName;
+    }
+
+    @Override
+    public byte[] processPayload(final byte[] currentPayload, final byte[] originalPayload, final byte[] baseValue) {
+        try {
+            byte[] result = RsaUtil.encrypt(currentPayload).getBytes("UTF-8");
+            parent.dict.Log(result, originalPayload);
+            return result;
+        } catch (Exception e) {
+            this.parent.callbacks.issueAlert(e.toString());
+            this.parent.stderr.println();
+            e.printStackTrace(this.parent.stderr);
+            return null;
+        }
+    }
+}
